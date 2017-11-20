abf42768946dfe8dfcd517cf0741309e77cbca11
Rameez Virji <rameez@bigtreeworld.com>
2017-11-19 20:13:12 -0600

    Add page header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { IntroSliderComponent } from './components/intro-slider/intro-slider.component';
 import { IntroSliderSlideComponent } from './components/intro-slider/slide.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, IntroSliderComponent, IntroSliderSlideComponent
+    AppComponent, IntroSliderComponent, IntroSliderSlideComponent, PageHeaderComponent
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule
---
+++ src/app/components/page-header/page-header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.scss']
+})
+export class PageHeaderComponent implements OnInit {
+    @Input() darkenScrollHeight = 0;
+    private hasHeaderScrolled = false;
+    ngOnInit(): void {
+        window.addEventListener('scroll', () => {
+            if (typeof window.pageYOffset === 'number') {
+                this.hasHeaderScrolled = window.pageYOffset > this.darkenScrollHeight;
+            }
+        });
+    }
+    get darkenHeader(): boolean {
+        return this.hasHeaderScrolled;
+    }
+}
