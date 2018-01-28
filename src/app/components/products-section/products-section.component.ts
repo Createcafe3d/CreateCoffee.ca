dc15a9cdddd3864374cebecab6d0be4f3a1356a7
Rameez Virji <rameez@bigtreeworld.com>
2018-01-27 21:01:19 -0600

    Paginate the products

--- src/app/components/products-section/products-section.component.ts
+++ src/app/components/products-section/products-section.component.ts
@@ -8,11 +8,43 @@ import { ShopifyService, Product } from '../../services/shopify/shopify.service'
 })
 export class ProductsSectionComponent implements OnInit {
   constructor(private shopifyService: ShopifyService) {}
+
+  currOffset = 0;
+  PAGE_SIZE = 6;
+
   ngOnInit(): void {
     this.shopifyService.fetchAllProducts();
   }
 
   get products(): Product[] {
-    return this.shopifyService.loadedProducts;
+    return this.shopifyService.loadedProducts.slice(this.currOffset, this.currOffset + this.PAGE_SIZE);
+  }
+
+  nextProducts(): void {
+    if (this.hasNext) {
+      this.currOffset += this.PAGE_SIZE - 1;
+    } else {
+      this.currOffset = 0;
+    }
+  }
+
+  prevProducts(): void {
+    if (this.hasPrev) {
+      if (this.currOffset < this.PAGE_SIZE) {
+        this.currOffset = 0;
+      } else {
+        this.currOffset -= (this.PAGE_SIZE - 1);
+      }
+    } else {
+      this.currOffset = this.shopifyService.loadedProducts.length - (this.PAGE_SIZE - 1);
+    }
+  }
+
+  get hasNext(): boolean {
+    return this.shopifyService.loadedProducts.length > this.currOffset + this.PAGE_SIZE - 1;
+  }
+
+  get hasPrev(): boolean {
+    return this.currOffset > 0;
   }
 }
